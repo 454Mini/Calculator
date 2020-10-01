6899d9de6e55490e3fc68c2fe127795d366f7501
Henriette Bjerre Vandt <henriette@vandtfoto.dk>
2020-10-01 11:01:20 +0200

    Commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult CalculatorView()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult CalculatorView(CalculatorModel model, string calc)
+        {
+            if(calc=="+")
+            {
+                model.Result = model.Input1 + model.Input2;
+            }
+          else if (calc == "-")
+            {
+                model.Result = model.Input1 - model.Input2;
+            }
+
+            else if (calc == "*")
+            {
+                model.Result = model.Input1 * model.Input2;
+            }
+
+            else if (calc == "/")
+            {
+                model.Result = model.Input1 / model.Input2;
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public double Input1 { get; set; }
+        public double Input2 { get; set; }
+        public double Result { get; set; }
+
+        public string Button { get; set; }
+
+        //public double calc ( string button, double input)
+        //{
+        //    this.Button = button;
+        //    switch (Button)
+        //    {
+        //        case "add":
+        //            Result = Input1 + Input2;
+        //            break;
+        //        case "sub":
+        //            Result = Input1 - Input2;
+        //            break;
+        //        case "mul":
+        //            Result = Input1 * Input2;
+        //            break;
+        //        case "Divi":
+        //            Result = Input1 / Input2;
+        //            break;
+        //    }
+            
+            
+        //    return Result;
+        //}
+    }
+}
